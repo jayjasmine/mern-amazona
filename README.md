4b6ac6d85e239afb993f2093555a25b1579b7675
Jay Jasmine <84842029+jayjasmine@users.noreply.github.com>
2022-11-21 11:22:57 +1000

    Added, rendered products on the screen and styling

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 # To Do
 
-1. Install Tools
-2. Create React App
-3. Create Git Repository
+1. ~~Install Tools~~
+2. ~~Create React App~~
+3. ~~Create Git Repository~~
+4. ~~Created data.js with products array, added images, render products on the screen and styling~~
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name:'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image:'/images/p1.jpg',
+      price: 39,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Perfect shirt for any season'
+    },
+    {
+      name:'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image:'/images/p2.jpg',
+      price: 45,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'Fit shirt that looks good all the time any time'
+    },
+    {
+      name:'Nike Slim Pants',
+      slug: 'nike-slim-pants',
+      category: 'Pants',
+      image:'/images/p3.jpg',
+      price: 70,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'Perfect pants for any season'
+    },
+    {
+      name:'Adidas Fit Pants',
+      slug: 'adidas-fit-pants',
+      category: 'pants',
+      image:'/images/p4.jpg',
+      price: 50,
+      countInStock: 5,
+      brand: 'Adidas',
+      rating: 4.2,
+      numReviews: 9,
+      description: 'Look fit all year long'
+    },
+  ],
+};
+
+export default data; 
