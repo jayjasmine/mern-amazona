682b7306ee757452b39d572d1bb49ea95e6d9536
Jay Jasmine <84842029+jayjasmine@users.noreply.github.com>
2022-12-06 10:03:24 +1000

    Added react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,35 @@
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
- 
+import NavBar from 'react-bootstrap/NavBar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to="/">amazona</Link>
+        <NavBar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <NavBar.Brand>amazona</NavBar.Brand>            
+            </LinkContainer>
+          </Container>
+        </NavBar>
+
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen />} />
           <Route path="/" element={<HomeScreen /> } />
         </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>All rights reservered</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
+// import logger from 'use-reducer-logger';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,7 +17,7 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     loading: true,
     error: '',
     products: [],
